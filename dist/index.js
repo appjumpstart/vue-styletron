4b7579f4c964754191f05c36536ff4b7edc08d3e
Ian Walter <public@iankwalter.com>
2017-09-05 09:49:40 -0400

    chore(compile): Compiling dist

--- dist/index.js
+++ dist/index.js
@@ -18,10 +18,10 @@ const styletron = new _styletron2.default()
 
 exports.default = config => {
   return {
-    config,
     install (Vue) {
+      Vue.$style = Vue.prototype.$style = config
       Vue.directive('style', (el, { value }) => {
-        const style = typeof value === 'function' ? value(this.config) : value
+        const style = typeof value === 'function' ? value(Vue.$style) : value
         el.className += ` ${(0, _styletronUtils.injectStyle)(styletron, style)}`
       })
     }
